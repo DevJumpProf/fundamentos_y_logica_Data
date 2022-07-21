2d72c0d4be4bccb6cc60c529bfdffc79216f0531
Arbusta <gabriel.munoz@arbusta.net>
2022-07-21 10:40:24 -0300

    ejercicios extra

---
+++ _CLASES_EJERCICIOS/_Clases/JS/Repaso.js
@@ -0,0 +1,22 @@
+
+//Operadores Matematicos
+
+suma = 10+10   // 20
+resta =20-10  //10
+multiplicacion= 5*5  // 25
+division = 20/2  // 10
+modulo= 7%2 //
+
+
+//prefijo
+contador = 10
+
+contador+=1   
+// contador = contador + 1
+
+document.write (contador)
+
+
+
+
+
